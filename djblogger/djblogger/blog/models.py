d806c805e90649277571f4772e1abc1e0df2b657
Sahil Chourasiya <schoursiaya127@gmail.com>
2023-02-27 12:19:36 +0530

    created model Post and to admin site

--- djblogger/djblogger/blog/models.py
+++ djblogger/djblogger/blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# Create Model for table Post
+class Post(models.Model):
+    options = (("draft", "Draft"), ("published", "Published"))
+
+    # Make title with charfield it requires max_length
+    title = models.CharField(max_length=250)
+    sub_title = models.CharField(max_length=100)
+    # slugfield in usable URLfield that does not accept spcial char
+    # we can set unique true so each entry is unique
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        # We use User model provided by django
+        # Cascade deletes entry from user table on delete
+        User,
+        on_delete=models.CASCADE,
+        related_name="post_author",
+    )
+    content = models.TextField()
+    # auto_now_add only run once
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    # auto_now run very time we update
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10, choices=options, default="draft"
+    )
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        # return human readable text
+        return self.name
